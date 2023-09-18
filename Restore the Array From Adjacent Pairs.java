5f368904889949befcef9694622b49a10c06788d
Rishabh Kumrawat <97178821+rishikumrawat02@users.noreply.github.com>
2023-09-18 19:21:30 +0530

    Update Restore the Array From Adjacent Pairs.java

--- Restore the Array From Adjacent Pairs.java
+++ Restore the Array From Adjacent Pairs.java
@@ -1,3 +1,4 @@
+// Solution-1
 class Solution {
     public int[] restoreArray(int[][] adjP) {
         int n=adjP.length;
@@ -35,5 +36,40 @@ class Solution {
             }
         }
         return;
+    }
+}
+
+// Solution-2
+class Solution {
+    public int[] restoreArray(int[][] adjP) {
+        int n=adjP.length;
+        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            int u=adjP[i][0]; int v=adjP[i][1];
+            map.putIfAbsent(u,new ArrayList<>());
+            map.putIfAbsent(v,new ArrayList<>());
+            map.get(u).add(v); map.get(v).add(u);
+        }
+        HashSet<Integer> vis = new HashSet<>();
+        int src=adjP[0][0];
+        for(int key: map.keySet()){
+            if(map.get(key).size()==1){
+                src=key; break;
+            }
+        }
+        int []res = new int[map.size()]; int i=0; 
+        while(!vis.contains(src)){
+            res[i++]=src; vis.add(src);
+            if(map.get(src).size()==1){
+                src=map.get(src).get(0);
+            }else{
+                if(vis.contains(map.get(src).get(0))){
+                    src=map.get(src).get(1);
+                }else{
+                    src=map.get(src).get(0);
+                }
+            }
+        }
+        return res;        
     }
 }
