a3375e9a9d99bd7cc3bd75d0f8dc41141054cf03
Rishabh Kumrawat <97178821+rishikumrawat02@users.noreply.github.com>
2023-09-27 10:33:33 +0530

    Update Minimum cash flow.java

--- Minimum cash flow.java
+++ Minimum cash flow.java
@@ -13,30 +13,12 @@ public class Solution
                 netAmnt[j]+=val; netAmnt[i]-=val;
             }
         } 
-        int res[] = {0};
-        helper(netAmnt,res);
-        return res[0];
-    }
-
-    static void helper(int netAmnt[], int res[]){
-        int minIdx=0; int maxIdx=0; int n=netAmnt.length;
-        for(int i=1; i<n; i++){
-            if(netAmnt[i]<netAmnt[minIdx]) minIdx=i;
-            if(netAmnt[i]>netAmnt[maxIdx]) maxIdx=i;
+        int res=0;
+        for(int i=0; i<n; i++){
+            if(netAmnt[i]>0){
+                res+=netAmnt[i];
+            }
         }
-        if(netAmnt[minIdx]==0 && netAmnt[maxIdx]==0) return;
-        if(Math.abs(netAmnt[minIdx])>=netAmnt[maxIdx]){
-            netAmnt[minIdx]+=netAmnt[maxIdx];
-            res[0]+=netAmnt[maxIdx];
-            netAmnt[maxIdx]=0;
-        }else{
-            netAmnt[maxIdx]-=Math.abs(netAmnt[minIdx]);
-            res[0]+=Math.abs(netAmnt[minIdx]);
-            netAmnt[minIdx]=0;
-        }
-        helper(netAmnt,res);
-        return;
-    }
-
-    
+        return res;
+    }    
 }
